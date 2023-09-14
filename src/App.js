fbe05493d809b40dda607800b459f3035d520ddf
pasindu <pasiweerarathne98@gmail.com>
2023-09-14 12:35:10 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import AddPost from "./features/posts/AddPost";
-import Posts from "./features/posts/Posts";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from './features/users/UserPage';
+import Layout from "./components/Layout";
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <main>
-      <AddPost />
-      <Posts />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        {/* Catch all - replace with 404 component if you want */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+
+      </Route>
+    </Routes>
   );
 }
 
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import postReducer from "../features/posts/postSlice";
-import userReducer from "../features/users/userSlice";
+import postsReducer from '../features/posts/postsSlice';
+import usersReducer from '../features/users/usersSlice';
+
 
 export const store = configureStore({
-  reducer: {
-    posts: postReducer,
-    users: userReducer,
-  },
-});
+    reducer: {
+        posts: postsReducer,
+        users: usersReducer
+    }
+})
