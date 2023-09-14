c0c1b19ce806e7175c963323eed2c275a0cf0b13
pasindu <pasiweerarathne98@gmail.com>
2023-09-14 19:33:47 +0530

    add changes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import { fetchPosts } from './features/posts/postsSlice';
-import { fetchUsers } from './features/users/usersSlice';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ApiProvider } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./features/api/apiSlice";
 
-store.dispatch(fetchPosts());
-store.dispatch(fetchUsers());
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </Router>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApiProvider api={apiSlice}>
+    <App />
+  </ApiProvider>
 );
